572769ec78e0d3b1674a47be6e1f284040812f5f
Ramon Rodriguez <ramonrdd3@gmail.com>
2019-06-25 20:15:54 -0600

    Tablas, alergia enfermedad actividad y medicamentos

---
+++ database/migrations/2019_06_26_015700_create_alergias_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAlergiasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('alergias', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre');
+            $table->string('categoria');
+            $table->string('reaccion');
+            $table->string('observaciones');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('alergias');
+    }
+}
