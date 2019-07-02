c93280fcbd22bb30fe4864f67c249ef4ab5a698e
falfaro12 <fabi.alfaro988@gmail.com>
2019-07-02 16:48:03 -0600

    hhhh

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -97,60 +97,29 @@ class AuthController extends Controller
 {
     try {
         $this->validate($request, [
-            'name' => 'required',
             'email' => 'required|email',
-            'password' => 'required|min:6'
-        ]);
-    } catch (\Illuminate\Validation\ValidationException $e ) {
-        return \response($e->errors(),422);
-    }
-    $user = new User();
-    $user->name = $request->name;
-    $user->email = $request->email;
-    $user->password = bcrypt($request->password);
-    $user->role_id=$request->role_id;
-    $user->save();
-    return response()->json(['user' => $user]);
-}
-
-    //Este es el registar un Paciente Asociado o Medico depende
-
-    public function registerAsociado(Request $request)
-    {
-        try {
-            $this->validate($request, [
-                'email' => 'required|email',
                 'nombre' => 'required|min:5',
                 'primerApellido' => 'required|min:6',
                 'segundoApellido' => 'required|min:6',
                 'sexo' => 'required|min:1',
                 'password' => 'required|min:6'
-
-            ]);
-        } catch (\Illuminate\Validation\ValidationException $e ) {
-            return \response($e->errors(),422);
-        }
-        if (!$user = JWTAuth::parseToken()->authenticate()) {
-           return response()->json(['msg'=>'Usuario no encontrado'], 404);
-        }
-
-        $user1 = new User();
-
-        $user1->email = $request->email;
-        $user1->nombre = $request->nombre;
-        $user1->primerApellido = $request->primerApellido;
-        $user1->segundoApellido = $request->segundoApellido;
-        $user1->sexo = $request->sexo;
-        $user1->password = bcrypt($request->password);
-
-
-        $user1->rol()->associate(2);
-       
-        $user1->save();
-        return response()->json(['user' => $user1]);
-
-
+        ]);
+    } catch (\Illuminate\Validation\ValidationException $e ) {
+        return \response($e->errors(),422);
     }
+    $user1 = new User();
+    $user1->email = $request->email;
+    $user1->nombre = $request->nombre;
+    $user1->primerApellido = $request->primerApellido;
+    $user1->segundoApellido = $request->segundoApellido;
+    $user1->sexo = $request->sexo;
+    $user1->password = bcrypt($request->password);
+
+    $user1->rol()->associate(2);
+
+    $user1->save();
+    return response()->json(['user' => $user1]);
+}
 
 
 }
