325d7bfc65b2c708e6bb4ffab9894ad50585d764
falfaro12 <fabi.alfaro988@gmail.com>
2019-07-02 15:47:45 -0600

    s

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,133 +1,114 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\User;
 use Illuminate\Http\Request;
-use JWTAuth;
-
-
-
 use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Controller;
 
 
 class AuthController extends Controller
 {
-   /**
+    /**
      * Create a new AuthController instance.
      *
      * @return void
      */
     public function __construct()
     {
+        // ahi donde dice login se agregan todas las acciones en las que no ocupan las credenciales y agregamos register
         $this->middleware('jwt.auth', ['except' => ['login','register']]);
     }
 
-
-    protected function guard()
-    {
+    protected function guard(){
         return Auth::guard('api');
     }
-/**
-* Get a JWT via given credentials.
-*
-* @return \Illuminate\Http\JsonResponse
-*/
- //Este es el registar normal de la pagina principal
- public function register(Request $request)
- {
-     try {
-         $this->validate($request, [
-             'correo' => 'required|email',
-             'nombre' => 'required|min:5',
-             'primerApellido' => 'required|min:6',
-             'segundoApellido' => 'required|min:6',
-             'sexo' => 'required|min:1',
-             'contrasenna' => 'required|min:6'
 
-         ]);
-     } catch (\Illuminate\Validation\ValidationException $e ) {
-         return \response($e->errors(),422);
-     }
+    /**
+     * Get a JWT via given credentials.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login()
+    {
+        $credentials = request(['email', 'password']);
 
-     $user = new User();
-
-     $user->correo = $request->correo;
-     $user->nombre = $request->nombre;
-     $user->primerApellido = $request->primerApellido;
-     $user->segundoApellido = $request->segundoApellido;
-     $user->sexo = $request->sexo;
-     $user->contrasenna = bcrypt($request->contrasenna);
-
-     //Asociar con roll
-     $user->rol()->associate(3);
-
-     $user->save();
-     return response()->json(['user' => $user]);
- }
-
-/**
-* Get a JWT via given credentials.
-*
-* @return \Illuminate\Http\JsonResponse
-*/
-public function login(Request $request)
-{
-
-    $credentials = request(['correo', 'contrasenna']);
-    $token = $this->guard()->attempt($credentials);
-        if (!$token ) {
-            return response()->json(['error' => 'NO AUTORIZADO'], 401);
+        //attempt autentifica con las credenciales si realmente existe este usuario
+        if (! $token = $this->guard()->attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
         }
 
         return $this->respondWithToken($token);
-}
+    }
 
-/**
-* Get the authenticated User.
-*
-* @return \Illuminate\Http\JsonResponse
-*/
-public function me()
-{
-    return response()->json($this->guard()->user());
-}
+    /**
+     * Get the authenticated User.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function me()
+    {
+        return response()->json($this->guard()->user());
+    }
 
-/**
-* Log the user out (Invalidate the token).
-*
-* @return \Illuminate\Http\JsonResponse
-*/
-public function logout()
-{
-    $this->guard()->logout();
-    return response()->json(['message' => 'Successfully logged out']);
-}
+    /**
+     * Log the user out (Invalidate the token).
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout()
+    {
+        $this->guard()->logout();
 
-/**
-* Refresh a token.
-*
-* @return \Illuminate\Http\JsonResponse
-*/
-public function refresh()
-{
-    return $this->respondWithToken($this->guard()->refresh());
-}
+        return response()->json(['message' => 'Successfully logged out']);
+    }
 
-/**
-* Get the token array structure.
-*
-* @param string $token
-*
-* @return \Illuminate\Http\JsonResponse
-*/
-protected function respondWithToken($token)
+    /**
+     * Refresh a token.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function refresh()
+    {
+        return $this->respondWithToken($this->guard()->refresh());
+    }
+
+    /**
+     * Get the token array structure.
+     *
+     * @param  string $token
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'user'=>$this->guard()->user(),
+            'token_type' => 'bearer',
+            'expires_in' => $this->guard()->factory()->getTTL() * 60
+        ]);
+    }
+
+    public function register(Request $request)
 {
-    return response()->json([
-    'access_token' => $token,
-    'user' => $this->guard()->user(),
-    'token_type' => 'bearer',
-    'expires_in' => $this->guard()->factory()->getTTL() * 60
-    ]);
+    try {
+        $this->validate($request, [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|min:6'
+        ]);
+    } catch (\Illuminate\Validation\ValidationException $e ) {
+        return \response($e->errors(),422);
+    }
+    $user = new User();
+    $user->name = $request->name;
+    $user->email = $request->email;
+    $user->password = bcrypt($request->password);
+    $user->role_id=$request->role_id;
+    $user->save();
+    return response()->json(['user' => $user]);
 }
 
     //Este es el registar un medico el admi
