8e141888be939a233ec7f9de9327e15a76e4256a
Ramon Rodriguez <ramonrdd3@gmail.com>
2019-07-01 20:10:32 -0600

    Creacion de tablas pivot

---
+++ database/migrations/2019_07_02_005427_expedientes_alergias.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ExpedientesAlergias extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('expediente_alergia', function (Blueprin $table){
+            $table->increments('id');
+            $table->integer('expediente_id')->unsigned();
+            $table->integer('alergia_id')->unsigned();
+            $table->timestamps();
+            //Llaves foraneas
+            $table->foreign('expediente_id')->references('id')->on('expedientes');
+            $table->foreign('alergia_id')->references('id')->on('alergias');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('expediente_alergia', function(Blueprint $table){
+            $table->dropForeign('expediente_alergia_expediente_id_foreign');
+            $table->dropColumn('expediente_id');
+            $table->dropForeign('expediente_alergia_alergia_id_foreign');
+            $table->dropColumn('alergia_id');
+        });
+        Schema::dropIfExists('expediente_alergia');
+    }
+}
---
+++ database/migrations/2019_07_02_010155_expedientes_deseases.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ExpedientesDeseases extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('expediente_desease', function (Blueprin $table){
+            $table->increments('id');
+            $table->integer('expediente_id')->unsigned();
+            $table->integer('desease_id')->unsigned();
+            $table->timestamps();
+            //Llaves foraneas
+            $table->foreign('expediente_id')->references('id')->on('expedientes');
+            $table->foreign('desease_id')->references('id')->on('deseases');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('expediente_desease', function(Blueprint $table){
+            $table->dropForeign('expediente_desease_expediente_id_foreign');
+            $table->dropColumn('expediente_id');
+            $table->dropForeign('expediente_desease_desease_id_foreign');
+            $table->dropColumn('desease_id');
+        });
+        Schema::dropIfExists('expediente_desease');
+    }
+}
