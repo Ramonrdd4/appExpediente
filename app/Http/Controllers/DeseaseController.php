0503cc9a0a115aedadf06b8b9603aafe2fff3884
Ramon Rodriguez <ramonrdd3@gmail.com>
2019-07-10 20:42:19 -0600

    Creacion de controladores y acciones store

---
+++ app/Http/Controllers/DeseaseController.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Desease;
+
+class DeseaseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        try{
+            $this -> validate($request, [
+                'nombre'=>'required|min:5',
+                'observaciones'=>'required',
+                'expediente'=>'required|numeric:9'
+            ]);
+            //Obtener el usuario autentificado actual
+            if(!$user = JWTAuth::parseToken()->authenticate()){
+                return response()->json(['msg'=>'Usuario no encontrado'],404);
+            }
+
+        }
+        catch (\Illuminate\Validation\ValidationException $e) {
+            return \response($e->errors(),422);
+        }
+
+        $desease = new Desease([
+            'nombre'=>$request->input('nombre'),
+            'observaciones'=>$request->input('observaciones')
+        ]);
+
+        if($desease->save()){
+            //Asociar con expediente
+            $desease->expedientes()->attach($request->input('expediente')=== null ? [] : $request->input('expediente'));
+
+            $response=[
+                'msg'=> 'Enfermedad registrada',
+                'desease'=> $desease
+            ];
+            return response()->json($response, 201);
+        }
+        $response=[
+            'msg'=>'Error durante el registro'
+        ];
+        return response()->json($response, 404);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
